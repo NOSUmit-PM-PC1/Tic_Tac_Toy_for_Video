eb310d724da63990eb3232e2e8293823067b4e4b
Мария <mahadrum@yandex.ru>
2024-11-26 12:40:11 +0300

    Добавьте файлы проекта.

---
+++ Tic_Tac_Toy/FormGame.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Tic_Tac_Toy
+{
+    partial class FormGame
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridViewGame = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewGame
+            // 
+            this.dataGridViewGame.AllowUserToAddRows = false;
+            this.dataGridViewGame.AllowUserToDeleteRows = false;
+            this.dataGridViewGame.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewGame.ColumnHeadersVisible = false;
+            this.dataGridViewGame.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewGame.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewGame.Location = new System.Drawing.Point(141, 80);
+            this.dataGridViewGame.Name = "dataGridViewGame";
+            this.dataGridViewGame.ReadOnly = true;
+            this.dataGridViewGame.RowHeadersVisible = false;
+            this.dataGridViewGame.RowTemplate.Height = 100;
+            this.dataGridViewGame.Size = new System.Drawing.Size(411, 381);
+            this.dataGridViewGame.TabIndex = 0;
+            this.dataGridViewGame.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewGame_CellClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(58, 518);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(106, 23);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "Очистить поле";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // FormGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 577);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.dataGridViewGame);
+            this.Name = "FormGame";
+            this.Text = "Крестики-нолики";
+            this.Load += new System.EventHandler(this.FormGame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGame)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewGame;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ Tic_Tac_Toy/FormGame.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toy
+{
+    public partial class FormGame : Form
+    {
+        static int n = 3;
+        int turn = 1;
+        int[,] field = new int[n, n];
+
+        void ShowField()
+        {
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    string temp = "";
+                    switch(field[row, col])
+                    { 
+                        case 1: temp = "x"; break; 
+                        case 2: temp = "o"; break;
+                    }
+                    dataGridViewGame.Rows[row].Cells[col].Value = temp;
+                }
+            }
+        }
+
+        public FormGame()
+        {
+            InitializeComponent();
+        }
+
+        private void FormGame_Load(object sender, EventArgs e)
+        {
+            dataGridViewGame.Rows.Add(3);
+        }
+
+        private void dataGridViewGame_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            field[e.RowIndex, e.ColumnIndex] = turn;
+            turn = turn % 2 + 1;
+            ShowField();
+            //dataGridViewGame[e.ColumnIndex, e.RowIndex].Value = "0";
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            field = new int[n, n];
+            ShowField();
+        }
+    }
+}
